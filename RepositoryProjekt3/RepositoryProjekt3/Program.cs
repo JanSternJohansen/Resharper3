0a3dc651ac89cfd954d385766a5552f9d71d6cc1
JanSternJohansen <jaj@tec.dk>
2018-01-03 11:38:29 +0100

    First commit sdfsadf Second

---
+++ RepositoryProjekt3/RepositoryProjekt3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RepositoryProjekt3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
