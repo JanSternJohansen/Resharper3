71480a7f4f429b4c550208e3dc60e42f689313e8
JanSternJohansen <jaj@tec.dk>
2018-01-03 11:26:17 +0100

    Add project files.

---
+++ Resharper3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Resharper3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
